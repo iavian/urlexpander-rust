0a476cc045ba8251fb3960edd25930a974baf5b2
Vijay Anand <iavian@Vijays-MacBook-Pro.local>
2022-10-03 15:19:57 +0530

    a

--- src/resolver.rs
+++ src/resolver.rs
@@ -16,9 +16,9 @@ pub async fn resolve(url: &str, prime: &bool) -> Result<String, reqwest::Error> 
     #[cfg(feature = "memcache")]
     {
         let stream =
-            TcpStream::connect("memcached.iavian.net:11211").expect("Failed to create stream");
+            TcpStream::connect("mcache.iavian.net:11211").expect("Failed to create stream");
         let mut cache = Protocol::new(AllowStdIo::new(stream));
-        let key = format!("4_{}", &url);
+        let key = format!("1-1{}", &url);
         if !prime {
             let value = cache.get(&key).await.unwrap_or_default();
             let value = String::from_utf8_lossy(&value);
@@ -37,28 +37,11 @@ pub async fn resolve(url: &str, prime: &bool) -> Result<String, reqwest::Error> 
     }
 }
 
-async fn _resolve(url: &str) -> Result<String, reqwest::Error> {
-    let mut headers = header::HeaderMap::new();
-    headers.insert(
-        "Referer",
-        header::HeaderValue::from_static("https://google.com"),
-    );
-    let client = ClientBuilder::new()
-        .timeout(Duration::new(20, 0))
-        .redirect(Policy::limited(10))
-        .default_headers(headers)
-        .build()?;
-    let resp = client.get(url).send().await?;
-    let url = resp.url();
-    let resolved_url = clean_query(url.as_str()).unwrap_or(String::from(url.as_ref()));
-    Ok(resolved_url)
-}
-
 async fn _resolve_meta(purl: &str) -> Result<String, reqwest::Error> {
     let mut headers = header::HeaderMap::new();
     headers.insert(
         "Referer",
-        header::HeaderValue::from_static("https://google.com"),
+        header::HeaderValue::from_static("https://google.com/"),
     );
     let client = ClientBuilder::new()
         .timeout(Duration::new(20, 0))
